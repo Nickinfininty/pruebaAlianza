24cf2e829548e2d644add49919dc8d0604a1addb
Nick <nick.dexsys@gmail.com>
2020-12-15 05:51:48 -0500

    Loggin

--- back-alianza/src/main/java/net/nick/BasicApplication.java
+++ back-alianza/src/main/java/net/nick/BasicApplication.java
@@ -5,6 +5,8 @@ import java.util.Date;
 import net.nick.model.User;
 import net.nick.repositories.UserRepository;
 import java.util.stream.Stream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,6 +16,9 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class BasicApplication {
 
+    static final Logger log =
+        LoggerFactory.getLogger(BasicApplication.class);
+    
 	public static void main(String[] args) {
 		SpringApplication.run(BasicApplication.class, args);
 	}
---
+++ back-alianza/src/main/java/net/nick/controllers/LoggingController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.nick.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Nick
+ */
+@RestController
+public class LoggingController {
+
+    Logger logger = LoggerFactory.getLogger(LoggingController.class);
+
+    @RequestMapping("/")
+    public String index() {
+        logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
+
+        return "Howdy! Check out the Logs to see the output...";
+    }
+}
